082b68acf698bc324d570f83d82321d96a8d0da7
shihabdider <shihabdider@gmail.com>
2023-12-17 07:53:09 -0500

    aider: Move inline JavaScript from oauth2-init.html to oauth2-init.js.
    # Aider chat conversation:
    
    USER: Why are you using inline scripting? You should keep all scripts to their respective files instead of adding them to the html document.
    
    ASSISTANT: To adhere to best practices and keep the scripts organized, we should move the inline JavaScript code from `oauth2-init.html` to the `oauth2-init.js` file. This will help maintain a clear separation between the structure (HTML), presentation (CSS), and behavior (JavaScript) of the web page.
    
    Here are the steps to move the inline script from `oauth2-init.html` to `oauth2-init.js`:
    
    1. Remove the inline script from `oauth2-init.html`.
    2. Add the event listener code to `oauth2-init.js`.
    
    Let's start with the changes to `oauth2-init.html`:
    
    oauth2-init.html
    ```html
    <<<<<<< SEARCH
        <script src="oauth2-init.js"></script>
        <script>
            document.getElementById('authorize_button').addEventListener('click', function() {
                chrome.runtime.sendMessage({action: 'authorize'});
            });
    
            document.getElementById('signout_button').addEventListener('click', function() {
                chrome.runtime.sendMessage({action: 'signout'});
            });
        </script>
    =======
        <!-- Inline script moved to oauth2-init.js -->
    >>>>>>> REPLACE
    ```
    
    Next, we'll add the event listener code to `oauth2-init.js`:
    
    oauth2-init.js
    ```javascript
    <<<<<<< SEARCH
    // This script will be responsible for handling the OAuth2 flow
    // It will communicate with the background script to initiate the authorization process
    =======
    // This script will be responsible for handling the OAuth2 flow
    // It will communicate with the background script to initiate the authorization process
    
    // Add event listeners to the buttons when the DOM is fully loaded
    document.addEventListener('DOMContentLoaded', function() {
        document.getElementById('authorize_button').addEventListener('click', function() {
            chrome.runtime.sendMessage({action: 'authorize'});
        });
    
        document.getElementById('signout_button').addEventListener('click', function() {
            chrome.runtime.sendMessage({action: 'signout'});
        });
    });
    >>>>>>> REPLACE
    ```
    
    With these changes, the JavaScript code is now properly organized within the `oauth2-init.js` file, and `oauth2-init.html` is free of inline scripts. This will make the code easier to maintain and debug.

--- oauth2-init.js
+++ oauth2-init.js
@@ -1,6 +1,17 @@
 // This script will be responsible for handling the OAuth2 flow
 // It will communicate with the background script to initiate the authorization process
 
+// Add event listeners to the buttons when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('authorize_button').addEventListener('click', function() {
+        chrome.runtime.sendMessage({action: 'authorize'});
+    });
+
+    document.getElementById('signout_button').addEventListener('click', function() {
+        chrome.runtime.sendMessage({action: 'signout'});
+    });
+});
+
 // No changes needed if the above code matches the existing code
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
