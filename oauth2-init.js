ce39e4fedd8066f210acce876cc14103677b9809
shihabdider <shihabdider@gmail.com>
2023-12-16 21:09:34 -0500

    aider: Created OAuth2 initiation page and JavaScript handler for OAuth2 flow, and updated manifest to include new resources.
    # Aider chat conversation:
    
    USER: I don't have anything test the OAuth flow yet, can you generate something?
    
    ASSISTANT: Certainly! To test the OAuth flow, we can create a simple HTML page that will initiate the authentication process with Google and request access to the user's Google Calendar. This will involve redirecting the user to the Google sign-in page, where they can grant the necessary permissions.
    
    We will need to create a new HTML file that will serve as the OAuth2 initiation page. This page will include a button or link that the user can click to start the OAuth flow.
    
    Here's the code for the new OAuth initiation page:
    
    oauth2-init.html
    ```html
    <<<<<<< SEARCH
    =======
    <!DOCTYPE html>
    <html lang="en">
    <head>
        <meta charset="UTF-8">
        <title>Google Calendar Integration</title>
        <script src="oauth2-init.js"></script>
    </head>
    <body>
        <h1>Google Calendar Integration</h1>
        <button id="authorize_button" style="display: none;">Authorize</button>
        <button id="signout_button" style="display: none;">Sign Out</button>
        <script>
            document.getElementById('authorize_button').onclick = function() {
                chrome.runtime.sendMessage({ action: 'authorize' });
            };
            document.getElementById('signout_button').onclick = function() {
                chrome.runtime.sendMessage({ action: 'signout' });
            };
        </script>
    </body>
    </html>
    >>>>>>> REPLACE
    ```
    
    Next, we need to create the JavaScript file that will handle the OAuth2 flow:
    
    oauth2-init.js
    ```javascript
    <<<<<<< SEARCH
    =======
    // This script will be responsible for handling the OAuth2 flow
    // It will communicate with the background script to initiate the authorization process
    
    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
        if (request.action === 'authorize') {
            chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
                console.log('Token acquired:', token);
                console.log('Token details:', chrome.identity.getAuthToken);
                // Here you would typically pass the token to your background script
                // and start interacting with the Google Calendar API.
            });
        } else if (request.action === 'signout') {
            chrome.identity.removeCachedAuthToken({ 'token': token }, function() {
                console.log('Token removed.');
                // Update UI or notify the user that they have signed out
            });
        }
    });
    >>>>>>> REPLACE
    ```
    
    We also need to update the `manifest.json` to include the new OAuth2 initiation page:
    
    manifest.json
    ```json
    <<<<<<< SEARCH
      "options_page": "record.html"
    =======
      "options_page": "record.html",
      "web_accessible_resources": [
        "audio/focus_compressed.opus",
        "oauth2-init.html",
        "oauth2-init.js"
      ]
    >>>>>>> REPLACE
    ```
    
    These changes will create the necessary files and update the manifest to include them. The OAuth2 initiation page will allow you to test the OAuth flow and ensure that the user can successfully authenticate and grant permissions to the extension to access their Google Calendar.
    
    Please add `oauth2-init.html` and `oauth2-init.js` to your project, and update the `manifest.json` file accordingly. Once you have these in place, you can load your extension in Chrome, navigate to the options page, and click the "Authorize" button to start the OAuth flow.

---
+++ oauth2-init.js
@@ -0,0 +1,18 @@
+// This script will be responsible for handling the OAuth2 flow
+// It will communicate with the background script to initiate the authorization process
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'authorize') {
+        chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+            console.log('Token acquired:', token);
+            console.log('Token details:', chrome.identity.getAuthToken);
+            // Here you would typically pass the token to your background script
+            // and start interacting with the Google Calendar API.
+        });
+    } else if (request.action === 'signout') {
+        chrome.identity.removeCachedAuthToken({ 'token': token }, function() {
+            console.log('Token removed.');
+            // Update UI or notify the user that they have signed out
+        });
+    }
+});
