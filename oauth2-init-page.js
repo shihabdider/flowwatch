c532a349a4be5766b09067d7296d472b751d916b
shihabdider <shihabdider@gmail.com>
2023-12-17 07:45:24 -0500

    fix: remove redundnt oauth js

--- oauth2-init-page.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('authorize_button').onclick = function() {
-    chrome.runtime.sendMessage({ action: 'authorize' });
-};
-document.getElementById('signout_button').onclick = function() {
-    chrome.runtime.sendMessage({ action: 'signout' });
-};
