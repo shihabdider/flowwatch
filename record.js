b547ba75652b88282f99aa5a59b9a1bcfeb71375
shihabdider <shihabdider@gmail.com>
2023-12-17 08:17:18 -0500

    Remove record table and CSV export functionality, as records are no longer stored locally.

--- record.js
+++ record.js
@@ -20,31 +20,4 @@ document.addEventListener('DOMContentLoaded', function () {
     newWindowToggle.addEventListener('change', () => {
         chrome.storage.sync.set({ 'newWindow': newWindowToggle.checked });
     });
-
-    // The clearRecords button functionality will be removed as we are not storing records locally anymore.
-    // This section has been removed as we will no longer display records in a table.
-    // Instead, we will create Google Calendar events.
-
-    function convertToCsv(records) {
-        const csvRows = [];
-        // Add header
-        csvRows.push('Date,Start Time,End Time,Duration');
-        // Add records
-        records.forEach(function(record) {
-            const startDate = new Date(record.startTime);
-            const endDate = new Date(record.endTime);
-            const dateStr = startDate.toLocaleDateString();
-            const startTimeStr = startDate.toLocaleTimeString();
-            const endTimeStr = endDate.toLocaleTimeString();
-            const duration = record.elapsedTime;
-            const hours = Math.floor(duration / 3600000);
-            const minutes = Math.floor((duration % 3600000) / 60000);
-            const seconds = Math.floor((duration % 60000) / 1000);
-            const durationStr = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-            csvRows.push(`${dateStr},${startTimeStr},${endTimeStr},${durationStr}`);
-        });
-        return csvRows.join('\n');
-    }
-
-    // The exportCsv button functionality will be removed as we are not storing records locally anymore.
 });
