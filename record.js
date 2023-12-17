843031eda266164b268bc40d4e90294a6714490f
shihabdider <shihabdider@gmail.com>
2023-12-17 08:18:51 -0500

    aider: Updated record.html and record.js to integrate OAuth functionality and add a switch for controlling Google Calendar recording.
    # Aider chat conversation:
    
    USER: I've added the oauth files.
    
    ASSISTANT: Great, now that the OAuth functionality is being moved to `record.html` and `record.js`, and you've added a switch to control the recording on Google Calendar, we can proceed with the necessary changes.
    
    First, we'll update `record.html` to include the new switch for controlling Google Calendar recording and the buttons for authorization and signout.
    
    record.html
    ```html
    <<<<<<< SEARCH
        <br>
        <script src="record.js"></script>
    </body>
    </html>
            <div class="switch-container">
                <label class="switch">
                    <input type="checkbox" id="recordCalendarToggle">
                    <span class="slider round"></span>
                </label>
                <span id="toggleLabel">Record on Google Calendar</span>
            </div>
            <button id="authorize_button">Authorize</button>
            <button id="signout_button" style="display: none;">Sign Out</button>
    =======
        <div class="switch-container">
            <label class="switch">
                <input type="checkbox" id="recordCalendarToggle">
                <span class="slider round"></span>
            </label>
            <span id="toggleLabel">Record on Google Calendar</span>
        </div>
        <div class="button-container">
            <button id="authorize_button">Authorize</button>
            <button id="signout_button" style="display: none;">Sign Out</button>
        </div>
        <script src="record.js"></script>
    </body>
    </html>
    >>>>>>> REPLACE
    ```
    
    Next, we'll update `record.js` to include the OAuth functionality that was previously in `oauth2-init.js`.
    
    record.js
    ```javascript
    <<<<<<< SEARCH
    document.addEventListener('DOMContentLoaded', function() {
        const recordCalendarToggle = document.getElementById('recordCalendarToggle');
    =======
    // This script will be responsible for handling the OAuth2 flow
    // It will communicate with the background script to initiate the authorization process
    
    document.addEventListener('DOMContentLoaded', function() {
        // Add event listeners to the buttons for OAuth
        document.getElementById('authorize_button').addEventListener('click', function() {
            chrome.runtime.sendMessage({action: 'authorize'});
        });
    
        document.getElementById('signout_button').addEventListener('click', function() {
            chrome.runtime.sendMessage({action: 'signout'});
        });
    
        // Update the display of the signout button based on the authorization state
        chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
            if (token) {
                document.getElementById('authorize_button').style.display = 'none';
                document.getElementById('signout_button').style.display = 'inline-block';
            } else {
                document.getElementById('authorize_button').style.display = 'inline-block';
                document.getElementById('signout_button').style.display = 'none';
            }
        });
    
        const recordCalendarToggle = document.getElementById('recordCalendarToggle');
    >>>>>>> REPLACE
    ```
    
    Please note that the above changes assume that the styles for `.button-container` are already defined in `record.css` to match the styling of the other elements on the page. If not, you may need to add the appropriate CSS.
    
    With these changes, the OAuth functionality is now integrated into the `record.html` and `record.js` files, and the switch for controlling Google Calendar recording is styled consistently with the rest of the options page.

--- record.js
+++ record.js
@@ -1,4 +1,27 @@
+// This script will be responsible for handling the OAuth2 flow
+// It will communicate with the background script to initiate the authorization process
+
 document.addEventListener('DOMContentLoaded', function() {
+    // Add event listeners to the buttons for OAuth
+    document.getElementById('authorize_button').addEventListener('click', function() {
+        chrome.runtime.sendMessage({action: 'authorize'});
+    });
+
+    document.getElementById('signout_button').addEventListener('click', function() {
+        chrome.runtime.sendMessage({action: 'signout'});
+    });
+
+    // Update the display of the signout button based on the authorization state
+    chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
+        if (token) {
+            document.getElementById('authorize_button').style.display = 'none';
+            document.getElementById('signout_button').style.display = 'inline-block';
+        } else {
+            document.getElementById('authorize_button').style.display = 'inline-block';
+            document.getElementById('signout_button').style.display = 'none';
+        }
+    });
+
     const recordCalendarToggle = document.getElementById('recordCalendarToggle');
 
     // Restore the state of the toggle from storage
